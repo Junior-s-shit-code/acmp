c014f40f11c8110c0a0eca46a17ea83c847fa82f
Dirwul <pwgofriends@gmail.com>
2020-07-10 03:07:50 +0500

    Dijkstra algorithm for each vertex; Problem with finding the shortest path to the vertex

---
+++ task_0562/solve.cpp
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+struct Vertex {
+
+	int v;
+
+	int dist;
+
+	bool operator <(const Vertex &second) const {
+		if (dist != second.dist) {
+			return dist > second.dist;
+		}
+		return v > second.v;
+	}
+};
+
+int main() {
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	int n, k;
+	scanf("%d %d", &n, &k);
+	const int INF = 100000;
+	std::vector <std::vector <int>> edges(n, std::vector <int>(n, INF));
+	for (int i = 0; i < n; i++) {
+		edges[i][i] = 0;
+	}
+	for (int i = 0; i < k; i++) {
+		int v1, v2;
+		scanf("%d %d", &v1, &v2);
+		v1--;
+		v2--;
+		edges[v1][v2] = 0;
+		edges[v2][v1] = std::min(edges[v2][v1], 1);
+	}
+
+	int ans = 0;
+	for (int from = 0; from < n; from++) {
+		std::vector <int> d = edges[from];
+		std::vector <bool> was(n, false);
+		std::priority_queue <Vertex> q;
+		q.push(Vertex { from, d[from] });
+
+		while (!q.empty()) {
+			Vertex min = q.top();
+			q.pop();
+
+			if (was[min.v]) {
+				continue;
+			}
+
+			if (min.dist == INF) {
+				break;
+			}
+
+			was[min.v] = true;
+			for (int to = 0; to < n; to++) {
+				int cost = edges[from][to];
+				if (cost == INF) {
+					continue;
+				}
+				if (min.dist + cost < d[to]) {
+					d[to] = min.dist + cost;
+					q.push(Vertex { to, d[to] });
+				}
+			}
+		}
+		for (int i = 0; i < n; i++) {
+			ans = std::max(ans, d[i]);
+			printf("%d ", d[i]);
+		}
+		printf("\n");
+	}
+	printf("%d", ans);
+	return 0;
+}
