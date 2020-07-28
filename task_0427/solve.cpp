ac45fb4f80ef3bfd75a5b804fa97281545b5e8ae
Dirwul <pwgofriends@gmail.com>
2020-07-28 17:28:06 +0500

    task_427

---
+++ task_0427/solve.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int n;
+    scanf("%d", &n);
+    std::vector <int> a(n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    std::sort(a.begin(), a.end());
+    long long curSum = 1;
+    for (int i = 0; i < n; i++) {
+        if (curSum < a[i]) {
+            printf("%lld", curSum);
+            return 0;
+        }
+        curSum += a[i];
+    }
+    printf("%lld", curSum);
+    return 0;
+}
