0e6a4aea34b47de6cd589bb46712423adc4c37c1
Dirwul <pwgofriends@gmail.com>
2020-09-04 02:53:40 +0500

    fix style-guide; rename map and vector

--- task_0393/solve.cpp
+++ task_0393/solve.cpp
@@ -18,23 +18,23 @@ int main() {
     freopen("output.txt", "w", stdout);
     int n;
     scanf("%d", &n);
-    std::unordered_map <std::string, Song> mp;
+    std::unordered_map <std::string, Song> songInfo;
     std::vector <int> ans(n);
     for (int i = 0; i < n; i++) {
         char buf[1 + 50];
         int time;
         scanf("%s %d", &buf, &time);
-        mp[std::string(buf)] = Song{ time, i };
+        songInfo[std::string(buf)] = Song{ time, i };
     }
     int nPlays, nSkip;
     scanf("%d %d", &nPlays, &nSkip);
-    std::vector <std::string> q(nPlays);
+    std::vector <std::string> songName(nPlays);
     for (int i = 0; i < nPlays; i++) {
-        char buf[1+50];
+        char buf[1 + 50];
         scanf("%s", &buf);
-        q[i] = std::string(buf);
+        songName[i] = std::string(buf);
     }
-    const int INF = 1000000000;
+    const int INF = (int)1e9;
     std::vector <int> skip(nSkip + 1, INF);
     for (int i = 0; i < nSkip; i++) {
         scanf("%d", &skip[i]);
@@ -43,20 +43,20 @@ int main() {
     int curTime = 0;
     int skipIndex = 0;
     for (int i = 0; i < nPlays; i++) {
-        curTime += mp[q[i]].time;
+        curTime += songInfo[songName[i]].time;
         if (skip[skipIndex] < curTime) {
-            ans[mp[q[i]].index]--;
+            ans[songInfo[songName[i]].index]--;
             curTime = skip[skipIndex];
             skipIndex++;
         } else if (skip[skipIndex] == curTime) {
             assert(i < nPlays - 1); //if we skip non-existent song
-            ans[mp[q[i]].index]++;
-            ans[mp[q[i + 1]].index]--;
+            ans[songInfo[songName[i]].index]++;
+            ans[songInfo[songName[i + 1]].index]--;
             curTime = skip[skipIndex];
             skipIndex++;
             i++;
         } else {
-            ans[mp[q[i]].index]++;
+            ans[songInfo[songName[i]].index]++;
         }
     }
     for (int i = 0; i < n; i++) {
