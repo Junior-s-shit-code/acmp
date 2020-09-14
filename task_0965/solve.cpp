62a040abe275014c03a60428c8703026c6c11ea5
Dirwul <pwgofriends@gmail.com>
2020-09-15 00:45:05 +0500

    task_965 1st sample test isn't correct
    использовал свою функцию popCount, т.к. __builtin_popcount
    а) не функция языка с++, а возможность компилятора gcc
    б) ребята с codeforces писали о том, что она не такая уж и быстрая, как должна быть
    в) ее аналоги, появившиеся в с++ 20 используют bitset, а это уже выглядит как слишком большое нагромождение и погоня за непонятно чем, кек
    г) неизвестно, используется ли этот стандарт (20 года) на acmp

---
+++ task_0965/solve.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+
+inline int popCount(int value) {
+    int ans = 0;
+    while (value) {
+        if (value & 1) {
+            ans++;
+        }
+        value >>= 1;
+    }
+    return ans;
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int nStation, nWays;
+    scanf("%d %d", &nStation, &nWays);
+    std::vector <int> way(nWays);
+    for (int i = 0; i < nWays; i++) {
+        int from, to;
+        scanf("%d %d", &from, &to);
+        from--;
+        to--;
+        way[i] = (1 << from) | (1 << to);
+    }
+    int best = nStation;
+    int bestCount = 0;
+    int bestCode = -1;
+
+    for (int code = 0; code < (1 << nStation); code++) {
+        bool isOk = true;
+        for (int iWay = 0; iWay < nWays; iWay++) {
+            if ((code & way[iWay]) == 0) {
+                isOk = false;
+                break;
+            }
+        }
+        if (isOk) {
+            int curBest = popCount(code);
+            if (curBest < best) {
+                best = curBest;
+                bestCount = 1;
+                bestCode = code;
+            } else if (curBest == best) {
+                bestCount++;
+            }
+        }
+    }
+
+    printf("%d %d\n", best, bestCount);
+    for (int i = 0; i < nStation; i++) {
+        if ((bestCode & (1 << i)) == 1) {
+            printf("%d", i + 1);
+        }
+    }
+    return 0;
+}
