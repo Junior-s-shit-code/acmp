4cbf1c7c66061286c741dbf6fb6f3331de6435f3
Dirwul <pwgofriends@gmail.com>
2020-09-16 02:26:18 +0500

    task_595 accepted

---
+++ task_0595/solve.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+std::vector <int> prefix_function(std::string &str) {
+    int n = (int)str.length();
+    std::vector <int> p(n);
+    for (int i = 1; i < n; i++) {
+        int j = p[i - 1];
+        while (j > 0 && str[i] != str[j]) {
+            j = p[j - 1];
+        }
+        if (str[i] == str[j]) {
+            j++;
+        }
+        p[i] = j;
+    }
+    return p;
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    char buf[1 + 50000];
+    char buf2[1 + 50000];
+    scanf("%s %s", &buf, &buf2);
+    std::string str(buf);
+    std::string sample(buf2);
+    std::string string = sample + "#" + str;
+    std::vector <int> p = prefix_function(string);
+    int rightLen = p[(int)p.size() - 1];
+    std::string toEqual = str.substr((int)str.length() - rightLen, rightLen);
+    std::string toReverse = str.substr(0, (int)str.length() - rightLen);
+    std::reverse(toReverse.begin(), toReverse.end());
+    std::string ans = toEqual + toReverse;
+    if (ans == sample) {
+        printf("Yes\n%d", (int)toReverse.size());
+    } else {
+        printf("No");
+    }
+    return 0;
+}
