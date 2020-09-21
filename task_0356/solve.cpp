aa4c291cce4fbed86ccd3b1961753d5e99a37e66
Dirwul <pwgofriends@gmail.com>
2020-09-21 19:52:13 +0500

    task_356 accepted

---
+++ task_0356/solve.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+struct Coin {
+
+	int cost;
+
+	int weight;
+
+	static Coin read() {
+		int cost, weight;
+		scanf("%d %d", &cost, &weight);
+		return Coin{ cost, weight };
+	}
+};
+
+int main() {
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	int freeWeight, maxWeight;
+	scanf("%d %d", &freeWeight, &maxWeight);
+	int weight = maxWeight - freeWeight;
+	int nCoins;
+	scanf("%d", &nCoins);
+	std::vector<Coin> coins(nCoins);
+	for (int i = 0; i < nCoins; i++) {
+		coins[i] = Coin::read();
+	}
+	const int INF = (int)1e9;
+	std::vector<int> maxCost(weight + 1, 0);
+	std::vector<int> minCost(weight + 1, INF);
+	minCost[0] = 0;
+	for (int i = 0; i <= weight; i++) {
+		if (minCost[i] != INF) {
+			for (int j = 0; j < nCoins; j++) {
+				int newInd = i + coins[j].weight;
+				if (newInd <= weight) {
+					minCost[newInd] = std::min(minCost[newInd], minCost[i] + coins[j].cost);
+					maxCost[newInd] = std::max(maxCost[newInd], maxCost[i] + coins[j].cost);
+				}
+			}
+		}
+	}
+
+	if (minCost[weight] == INF) {
+		printf("This is impossible.");
+	} else {
+		printf("%d %d", minCost[weight], maxCost[weight]);
+	}
+	return 0;
+}
