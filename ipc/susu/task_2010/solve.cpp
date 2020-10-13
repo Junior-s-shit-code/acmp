81220cee1b04bcd9756c2a2c74dfa8dcbc000308
Dirwul <pwgofriends@gmail.com>
2020-10-13 19:30:08 +0500

    accepted

--- ipc/susu/task_2010/solve.cpp
+++ ipc/susu/task_2010/solve.cpp
@@ -2,55 +2,52 @@
 #include <vector>
 #include <cmath>
 #include <algorithm>
+#include <queue>
+
+struct Board {
+
+    int len;
+
+    int slits = 0;
+
+    double segment;
+
+    bool operator<(const Board &second) const{
+        return segment < second.segment;
+    }
+};
 
 int main() {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
-    const int MULT = (int)1e4;
-    int n, maxDist;
-    scanf("%d %d", &n, &maxDist);
-    maxDist *= MULT;
-    std::vector<int> a(n);
+    int n, dist;
+    scanf("%d %d", &n, &dist);
+    std::priority_queue<Board> q;
+    double minValue = (double)1e9;;
     for (int i = 0; i < n; i++) {
-        scanf("%d", &a[i]);
-        a[i] *= MULT;
+        int value;
+        scanf("%d", &value);
+        minValue = std::min(minValue, (double)value);
+        q.push(Board{ value, 0, 1.0 * value });
     }
-    std::sort(a.begin(), a.end());
-    int bestAns = (int)1e9;
-    for (int startD = 1; startD <= 1000; startD++) {
-        int curDivisor = startD;
-        int curAns = curDivisor;
-        int minValue = a[0] / startD;
-        bool isGoNext;
-        for (int i = 1; i < n; i++) {
-            isGoNext = false;
-            while (true) {
-                int seg = a[i] / curDivisor;
-                int diff = std::abs(seg - minValue);
-                if (diff <= maxDist) {
-                    isGoNext = true;
-                    curAns += curDivisor;
-                    if (seg < minValue) {
-                        minValue = seg;
-                    }
-                    break;
-                } else if (seg < minValue) {
-                    break;
-                }
-                curDivisor++;
-                if (curDivisor > 1000) {
-                    break;
-                }
-            }
-            if (!isGoNext) {
-                break;
-            }
-        }
-        if (isGoNext) {
-            bestAns = curAns;
+    const double eps = (double)1e-6;
+    int ans = 0;
+    while (true) {
+        Board cur = q.top();
+        q.pop();
+        double diff = cur.segment - minValue;
+        if (std::abs(diff) < dist + eps) {
             break;
+        } else {
+            ans++;
+            cur.slits++;
+            cur.segment = 1.0 * cur.len / (cur.slits + 1);
+            if (cur.segment < minValue) {
+                minValue = cur.segment;
+            }
+            q.push(cur);
         }
     }
-    printf("%d", bestAns - n);
+    printf("%d", ans);
     return 0;
 }
