73291313760099108857f1d94aee2049d81c6ee3
Dirwul <pwgofriends@gmail.com>
2020-11-02 16:51:16 +0500

    rewrite without structure

--- ipc/susu/task_2428/solve.cpp
+++ ipc/susu/task_2428/solve.cpp
@@ -2,62 +2,42 @@
 #include <vector>
 #include <algorithm>
 
-struct Stick {
-
-	int value;
-
-	int count;
-};
-
 int main() {
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
 	int n;
 	scanf("%d", &n);
-	std::vector<int> a(n);
+	std::vector<int> len(n);
 	for (int i = 0; i < n; i++) {
-		scanf("%d", &a[i]);
+		scanf("%d", &len[i]);
 	}
-	std::sort(a.begin(), a.end());
-	std::vector<Stick> sticks;
-	for (int i = 0; i < n; i++) {
-		Stick newStick = { a[i], 1 };
-		while (i + 1 < n && a[i + 1] == a[i]) {
-			newStick.count++;
-			i++;
-		}
-		newStick.count -= (newStick.count & 1);
-		if (newStick.count > 0) {
-			sticks.push_back(newStick);
-		}
-	}
+	std::sort(len.begin(), len.end());
+	int curCount = 0;
+	int prevCount = 0;
+	int curValue = 0;
+	int prevValue = 0;
 	int ans = 0;
-	int iPrev = 0;
-	for (int i = 0; i < (int)sticks.size(); i++) {
-		while (true) {
-			if (sticks[iPrev].count == 0 || sticks[iPrev].value * 2 < sticks[i].value) {
-				iPrev++;
-				continue;
-			}
-
-			if (iPrev < i) {
-				int counter = std::min(sticks[iPrev].count, sticks[i].count);
-				sticks[iPrev].count -= counter;
-				sticks[i].count -= counter;
-				ans += (counter >> 1);
-			} else if (iPrev == i) {
-				int counter = sticks[i].count / 4;
-				sticks[i].count -= counter * 4;
-				ans += counter;
-			}
-
-			if (sticks[i].count == 0 || (sticks[i].count == 2 && i == iPrev)) {
-				break;
-			} else {
-				iPrev++;
-			}
+	for (int i = 0; i < n; i++) {
+		if (curValue != len[i] && curCount >= 2) {
+			prevValue = curValue;
+			prevCount = curCount;
+			curCount = 0;
+			curValue = len[i];
+		} else if (curValue != len[i]) {
+			curValue = len[i];
+			curCount = 0;
+		}
+		curCount++;
+		if (curCount >= 2 && prevCount >= 2 && prevValue * 2 >= curValue) {
+			curCount -= 2;
+			prevCount -= 2;
+			ans++;
+		} else if (curCount >= 4) {
+			curCount -= 4;
+			ans++;
 		}
 	}
+	
 	printf("%d", ans);
 	return 0;
 }
