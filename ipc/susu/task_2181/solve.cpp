bf002b64dcf95157f7b2a6c9e2826921a21d5240
Dirwul <pwgofriends@gmail.com>
2020-11-03 02:06:36 +0500

    rewrite with bitwise arithmetic

--- ipc/susu/task_2181/solve.cpp
+++ ipc/susu/task_2181/solve.cpp
@@ -1,39 +1,27 @@
 #include <stdio.h>
-#include <cmath>
-#include <algorithm>
-#include <vector>
-#include <string>
 
-std::string toBin(int num) {
-    std::string str = "";
-    while (num) {
-        str.insert(str.begin(), (num & 1) + '0');
-        num >>= 1;
-    }
-    return str;
-}
 
 int main() {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
     int n, a, b;
     scanf("%d %d %d", &n, &a, &b);
-    if (a < b) {
-        std::swap(a, b);
-    }
-    std::string str1 = toBin(a);
-    std::string str2 = toBin(b);
-    while ((int)str2.length() < (int)str1.length()) {
-        str2.insert(str2.begin(), '0');
-    }
-    int ans = -1;
-    bool isTwo = false;
-    for (int i = 0; i < (int)str1.length(); i++) {
-        int value = str1[i] + str2[i] - 2 * '0';
-        if (value > 0) {
-            ans = i;
+    int remainN = (1 << n) - a - b;
+    int minCount = 0;
+    int unusedBits = 1;
+    for (int i = n; i >= 0; i--) {
+        if (((a >> i) & 1) != 0) {
+            unusedBits--;
         }
+        if (((b >> i) & 1) != 0) {
+            unusedBits--;
+        }
+        if (((remainN >> 1) & 1) != 0) {
+            unusedBits--;
+        }
+        minCount += unusedBits;
+        unusedBits <<= 1;
     }
-    printf("%d", ans + 1);
+    printf("%d", minCount);
     return 0;
 }
