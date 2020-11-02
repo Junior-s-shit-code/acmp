4aff985bfa8ca5a2b4bbc672042d0c1cab9a85e2
Dirwul <pwgofriends@gmail.com>
2020-11-03 02:16:27 +0500

    a bit fix algo

--- ipc/susu/task_2174/solve.cpp
+++ ipc/susu/task_2174/solve.cpp
@@ -7,10 +7,7 @@ int main() {
     freopen("output.txt", "w", stdout);
     int a, b, c;
     scanf("%d %d %d", &a, &b, &c);
-    int diff1 = std::abs(a - b);
-    int diff2 = std::abs(b - c);
-    int diff3 = std::abs(a - c);
-    if (std::max({ diff1, diff2, diff3 }) <= 25) {
+    if (std::max({ a, b, c }) - std::min({a, b, c}) <= 25) {
         printf("ALLOWED");
     } else {
         printf("FORBIDDEN");
