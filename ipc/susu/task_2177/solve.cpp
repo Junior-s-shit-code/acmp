3f197a373ac4ecd238925d4bf1476297421904fd
Dirwul <pwgofriends@gmail.com>
2020-11-03 02:11:36 +0500

    ans -> midId, a bit change in algo

--- ipc/susu/task_2177/solve.cpp
+++ ipc/susu/task_2177/solve.cpp
@@ -32,9 +32,9 @@ int main() {
         p[i] = Point::read();
     }
     double bestDist = (double)1e9;
-    int ans1, ans2;
-    for (int capital1 = 1; capital1 < n; capital1++) {
-        for (int capital2 = 0; capital2 < capital1; capital2++) {
+    int midId1, midId2;
+    for (int capital2 = 1; capital2 < n; capital2++) {
+        for (int capital1 = 0; capital1 < capital2; capital1++) {
             Point c1 = p[capital1];
             Point c2 = p[capital2];
             double curDist = 0;
@@ -45,14 +45,11 @@ int main() {
             }
             if (curDist < bestDist) {
                 bestDist = curDist;
-                ans1 = capital1;
-                ans2 = capital2;
+                midId1 = capital1;
+                midId2 = capital2;
             }
         }
     }
-    if (ans1 > ans2) {
-        std::swap(ans1, ans2);
-    }
-    printf("%d %d", ans1 + 1, ans2 + 1);
+    printf("%d %d", midId1 + 1, midId2 + 1);
     return 0;
 }
