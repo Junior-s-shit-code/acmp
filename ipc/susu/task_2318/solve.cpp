ac6aa4cb9ef72f4030e177263e080d2614336e1d
Dirwul <pwgofriends@gmail.com>
2020-11-04 18:23:27 +0500

    fix braces position

--- ipc/susu/task_2318/solve.cpp
+++ ipc/susu/task_2318/solve.cpp
@@ -2,33 +2,30 @@
 #include <algorithm>
 
 int main() {
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-	int start1, end1, start2, end2;
-	scanf("%d %d %d %d", &start1, &end1, &start2, &end2);
-	int ans;
-	if (((start2 <= start1 && start1 <= end2) ||
-		 (start1 <= start2 && end2 <= start1)) &&
-		((start1 < end1 && start2 < end2) ||
-		 (end1 < start1 && end2 < start2))) {
-
-		if ((start2 <= end1 && end1 <= end2) ||
-			(end1 <= start2 && end2 <= end1)) {
-			ans = std::abs(start1 - start2) + 
-				std::abs(end1 - start1) + 2;
-		} else {
-			ans = std::abs(start1 - start2) + 
-				std::abs(end2 - start1) +
-				std::abs(end1 - end2) + 3;
-		}
-	} else {
-		ans = std::abs(end2 - start2) + 
-			std::abs(start1 - end2) + 
-			std::abs(end1 - start1) + 3;
-	}
-	if (start1 == end2) {
-		ans--;
-	}
-	printf("%d", ans);
-	return 0;
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int start1, end1, start2, end2;
+    scanf("%d %d %d %d", &start1, &end1, &start2, &end2);
+    int ans;
+    if ((start2 <= start1 && start1 <= end2 || start1 <= start2 && end2 <= start1) &&
+        (start1 < end1 && start2 < end2 || end1 < start1 && end2 < start2)
+    ) {
+        if (start2 <= end1 && end1 <= end2 || end1 <= start2 && end2 <= end1) {
+            ans = std::abs(start1 - start2) +
+                std::abs(end1 - start1) + 2;
+        } else {
+            ans = std::abs(start1 - start2) +
+                std::abs(end2 - start1) +
+                std::abs(end1 - end2) + 3;
+        }
+    } else {
+        ans = std::abs(end2 - start2) +
+            std::abs(start1 - end2) +
+            std::abs(end1 - start1) + 3;
+    }
+    if (start1 == end2) {
+        ans--;
+    }
+    printf("%d", ans);
+    return 0;
 }
