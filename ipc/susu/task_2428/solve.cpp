b87a762288fa992132fb439cd28f1d8c981903a7
Dirwul <pwgofriends@gmail.com>
2020-11-09 21:13:51 +0500

    rewrite and optimized code

--- ipc/susu/task_2428/solve.cpp
+++ ipc/susu/task_2428/solve.cpp
@@ -12,32 +12,26 @@ int main() {
 		scanf("%d", &len[i]);
 	}
 	std::sort(len.begin(), len.end());
-	int curCount = 0;
-	int prevCount = 0;
-	int curValue = 0;
-	int prevValue = 0;
-	int ans = 0;
-	for (int i = 0; i < n; i++) {
-		if (curValue != len[i] && curCount >= 2) {
-			prevValue = curValue;
-			prevCount = curCount;
-			curCount = 0;
-			curValue = len[i];
-		} else if (curValue != len[i]) {
-			curValue = len[i];
-			curCount = 0;
-		}
-		curCount++;
-		if (curCount >= 2 && prevCount >= 2 && prevValue * 2 >= curValue) {
-			curCount -= 2;
-			prevCount -= 2;
-			ans++;
-		} else if (curCount >= 4) {
-			curCount -= 4;
-			ans++;
+	int prevPairLen = 0;
+	int maxCount = 0;
+
+	for (int i = 0; i + 1 < n; ) {
+		if (len[i] == len[i + 1]) {
+			if (prevPairLen == 0) {
+				prevPairLen = len[i];
+			} else {
+				if (len[i] <= 2 * prevPairLen) {
+					maxCount++;
+					prevPairLen = 0;
+				} else {
+					prevPairLen = len[i];
+				}
+			}
+			i += 2;
+		} else {
+			i++;
 		}
 	}
-	
-	printf("%d", ans);
+	printf("%d", maxCount);
 	return 0;
 }
