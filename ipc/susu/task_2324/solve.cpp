2351b8718096d05c205a77059d4c3b07a8788878
Dirwul <pwgofriends@gmail.com>
2020-11-10 00:39:54 +0500

    UNDEFIND -> UNDIFINED

--- ipc/susu/task_2324/solve.cpp
+++ ipc/susu/task_2324/solve.cpp
@@ -7,19 +7,19 @@ int main() {
 	freopen("output.txt", "w", stdout);
 	const int MOD = (int)1e6;
 	const int INF = (int)1e9;
-	const int UNDEFIND = -1;
+	const int UNDEFINED = -1;
 	int n;
 	scanf("%d", &n);
-	std::vector<int> r(MOD, UNDEFIND);
+	std::vector<int> r(MOD, UNDEFINED);
 	r[0] = 0;
 	int curSum = 0;
 	int bestLen = INF;
-	int startI = UNDEFIND;
+	int startI = UNDEFINED;
 	for (int i = 1; i <= n; i++) {
 		int value;
 		scanf("%d", &value);
 		curSum = (curSum + value) % MOD;
-		if (r[curSum] != UNDEFIND) {
+		if (r[curSum] != UNDEFINED) {
 			int curLen = i - r[curSum];
 			if (curLen < bestLen) {
 				bestLen = curLen;
@@ -28,7 +28,7 @@ int main() {
 		}
 		r[curSum] = i;
 	}
-	if (startI == UNDEFIND) {
+	if (startI == UNDEFINED) {
 		printf("-1");
 	} else {
 		printf("%d %d", bestLen, startI + 1);
