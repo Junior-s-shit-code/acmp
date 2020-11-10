06e0d5126d52c2fb49afeb8b4e1ea9a4aab3b1f2
Dirwul <pwgofriends@gmail.com>
2020-11-10 23:38:05 +0500

    rewrite solve

--- ipc/susu/task_1521/solve.cpp
+++ ipc/susu/task_1521/solve.cpp
@@ -1,66 +1,70 @@
 #include <stdio.h>
 #include <vector>
 #include <algorithm>
+#include <cassert>
 
-inline int binSearch(const int value, int &curH, const std::vector<int> &s) {
-    int left = 0;
-    int right = (int)s.size();
-    while (left + 1 < right) {
-        int mid = ((left + right) >> 1);
-        if (s[mid] <= value) {
-            left = mid;
-        } else {
-            right = mid;
+const int LIMIT = (int)1e8;
+
+struct Pair {
+
+    int a;
+
+    int b;
+
+    int sum;
+};
+
+void generatePairs(std::vector<Pair> &pairs) {
+    const int MAX_PYRAMIDAL_ID = 668;
+    const int MAX_TRIANGULAR_ID = 843;
+    for (int a = 1; a <= MAX_PYRAMIDAL_ID; a++) {
+        int squareValue = a * (a + 1) * (2 * a + 1) / 6;
+        for (int b = 1; b <= MAX_TRIANGULAR_ID; b++) {
+            int triangleValue = b * (b + 1) * (b + 2) / 6;
+            int sum = squareValue + triangleValue;
+            if (sum > LIMIT) {
+               break;
+            }
+            pairs.push_back(Pair{ a, b, sum });
         }
     }
-    curH = left;
-    return s[left];
+}
+
+int binSearch(int value, const std::vector<Pair> &pairs) {
+    int left = 0;
+    int right = (int)pairs.size();
+    while (left + 1 < right) {
+        int mid = (left + right) >> 1;
+        if (pairs[mid].sum > value) {
+            right = mid;
+        } else {
+            left = mid;
+        }
+    }
+    return left;
 }
 
 int main() {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
-    const int LIMIT = (int)1e8;
-
-    std::vector<int> b = { 0 };
-
-    int value = 1;
-    int curSum = 0;
-    for (int value = 1, index = 1; curSum <= LIMIT; index++, value += index) {
-        curSum += value;
-        b.push_back(curSum);
-    }
-
+    std::vector<Pair> pairs;
+    generatePairs(pairs);
+    std::sort(pairs.begin(), pairs.end(), [](const Pair &left, const Pair &right) {
+        if (left.sum == right.sum) {
+            return left.a > right.a;
+        }
+        return left.sum < right.sum;
+    });
     while (true) {
+        int value;
         scanf("%d", &value);
         if (value == 0) {
             break;
         }
-        int bestAns = (int)1e8;
-        int bestFirst = -1;
-        int bestSecond = -1;
-        int curSum = 0;
-        int add = 1;
-        for (int i = 1; i < 671; i++) {
-            curSum += add * add;
-            add++;
-            int remain = value - curSum;
-            if (remain <= 0) {
-                break;
-            } else {
-                int curH;
-                int second = binSearch(remain, curH, b);
-                if (remain - second < bestAns) {
-                    bestAns = remain - second;
-                    bestFirst = i;
-                    bestSecond = curH;
-                    if (bestAns == 0) {
-                        break;
-                    }
-                }
-            }
-        }
-        printf("%d %d\n", bestFirst, bestSecond);
+        int index = binSearch(value, pairs);
+        assert(0 <= index && index < (int)pairs.size());
+        Pair ans = pairs[binSearch(value, pairs)];
+        printf("%d %d\n", ans.a, ans.b);
     }
     return 0;
 }
