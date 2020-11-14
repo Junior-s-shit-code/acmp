ec3b6a8beb0b86968f4590edd800a98e0d2e8df7
Dirwul <pwgofriends@gmail.com>
2020-11-14 19:44:06 +0500

    ipc.susu_1554 + rename + replace folders accepted

---
+++ ipc_susu/task_1554/solve.cpp
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <set>
+
+const int INF = (int)1e8;
+
+void findCutPoints(int v, int parent, 
+         int &curTime,
+         std::vector<int> &time,
+         std::vector<int> &minTime,
+         const std::vector<std::vector<int>> &g, 
+         std::set<int> &ans
+) {
+    time[v] = minTime[v] = curTime;
+    curTime++;
+    int childCount = 0;
+    for (int next : g[v]) {
+        if (next == parent) {
+            continue;
+        }
+        if (time[next] < INF) {
+            minTime[v] = std::min(minTime[v], minTime[next]);
+        } else {
+            findCutPoints(next, v, curTime, time, minTime, g, ans);
+            minTime[v] = std::min(minTime[v], minTime[next]);
+            childCount++;
+            if (parent < 0 && childCount > 1 ||
+                parent >= 0 && minTime[next] >= time[v]) {
+                ans.insert(v);
+            }
+        }
+    }
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int nV, nE;
+    scanf("%d %d", &nV, &nE);
+    std::vector<std::vector<int>> g(nV);
+    for (int iE = 0; iE < nE; iE++) {
+        int from, to;
+        scanf("%d %d", &from, &to);
+        from--;
+        to--;
+        g[from].push_back(to);
+        g[to].push_back(from);
+    }
+
+    std::vector<int> time(nV, INF), minTime(nV, INF);
+    std::set<int> ans;
+    int curTime = 0;
+    findCutPoints(0, -1, curTime, time, minTime, g, ans);
+    printf("%d\n", (int)ans.size());
+    for (int item : ans) {
+        printf("%d ", item + 1);
+    }
+    return 0;
+}
