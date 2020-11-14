26ba8334af4e4bdc85fb1d429e81db6f995a7b9e
Dirwul <pwgofriends@gmail.com>
2020-11-14 10:03:39 +0500

    faster solve

--- ipc/susu/task_1885/solve.cpp
+++ ipc/susu/task_1885/solve.cpp
@@ -1,28 +1,35 @@
 #include <stdio.h>
-#include <vector>
-#include <algorithm>
 #include <string>
+#include <vector>
 
 int main() {
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
     const int MOD = (int)1e9 + 9;
     char buf[1 + 100];
     scanf("%s", &buf);
-    std::string str(buf);
-    int n = (int)str.length();
-    std::vector<int> ans(n + 1, 0); 
-    ans[0] = ans[1] = 1;
-    for (int len = 2; len <= n; len++) {
-        std::string rStr = "";
-        for (int palindromLen = 1; palindromLen <= len; palindromLen++) {
-            rStr += str[len - palindromLen];
-            if (rStr == str.substr(len - palindromLen, palindromLen)) {
-                ans[len] += ans[len - palindromLen];
-                ans[len] %= MOD;
+    std::string s(buf);
+    int n = (int)s.length();
+    std::vector<std::vector<bool>> isPal(1 + n, std::vector<bool>(1 + n, false));
+    for (int iLen = 1; iLen <= n; iLen++) {
+        for (int iLeft = 1; iLeft + iLen - 1 <= n; iLeft++) {
+            int iRight = iLeft + iLen - 1;
+            if (iLen == 1) {
+                isPal[iLeft][iRight] = true;
+            } else if (iLen == 2) {
+                isPal[iLeft][iRight] = s[iLeft - 1] == s[iRight - 1];
+            } else {
+                isPal[iLeft][iRight] = s[iLeft - 1] == s[iRight - 1] && isPal[iLeft + 1][iRight - 1];
             }
         }
     }
-    printf("%d", ans[n]);
+    std::vector<int> countPartitions(1 + n);
+    countPartitions[0] = 1;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            if (isPal[j][i]) {
+                countPartitions[i] = (countPartitions[i] + countPartitions[j - 1]) % MOD;
+            }
+        }
+    }
+    printf("%d", countPartitions[n]);
     return 0;
 }
