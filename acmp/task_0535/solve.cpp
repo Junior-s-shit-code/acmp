7c70a3f79402116d01b7bec358fe2fd7be50f2a2
Dirwul <pwgofriends@gmail.com>
2020-11-26 17:22:06 +0500

    task_535 accepted

---
+++ acmp/task_0535/solve.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string>
+#include <set>
+
+long long getSum(long long num1, long long num2) {
+    std::string ans = "";
+    while (num1 + num2 > 0) {
+        int sum = num1 % 10 + num2 % 10;
+        ans = std::to_string(sum) + ans;
+        num1 /= 10;
+        num2 /= 10;
+    }
+    return std::atoll(ans.c_str());
+}
+
+long long getTripleSum(long long num1, long long num2, long long num3) {
+    return getSum(getSum(num1, num2), num3);
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    std::set<long long> ans;
+    ans.insert(getTripleSum(a, b, c));
+    ans.insert(getTripleSum(a, c, b));
+    ans.insert(getTripleSum(b, c, a));
+    printf((int)ans.size() == 1 ? "NO\n" : "YES\n");
+    for (long long item : ans) {
+        printf("%lld\n", item);
+    }
+    return 0;
+}
