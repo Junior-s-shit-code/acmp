dae15dbd0e53dd807480dfce259993d45a47bdd3
Dirwul <pwgofriends@gmail.com>
2020-11-26 16:42:55 +0500

    remove getting substring, fix code style

--- acmp/task_0536/solve.cpp
+++ acmp/task_0536/solve.cpp
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <string>
 #include <vector>
-#include <algorithm>
 #include <cstdlib>
 
 int main() {
@@ -14,31 +13,36 @@ int main() {
     for (int i = 0; i < ext; i++) {
         mod *= 10;
     }
-    std::vector <long long> a(n + 1, 0);
+    std::vector<long long> a(n + 1, 0);
     char buf[1 + 50000];
     scanf("%s", &buf);
     std::string s(buf);
+
+    std::string str;
     for (int len = 1; len < MAX_LEN && len <= n; len++) {
-        std::string str = s.substr(0, len);
+        str.insert(str.end(), s[len - 1]);
         if (str.length() > 1 && str[0] == '0' ||
             std::atoll(str.c_str()) > max
-        ) {
+            ) {
             continue;
         }
         a[len] = 1;
     }
+
     for (int i = 1; i <= n; i++) {
+        long long newValue = 0;
+        long long ext = 1;
         for (int j = i; j > 0 && i - j < MAX_LEN; j--) {
-            std::string str = s.substr(j - 1, i - j + 1);
-            long long newVal = std::atoll(str.c_str());
-            if (newVal > max) {
+            int add = s[j - 1] - '0';
+            newValue = add * ext + newValue;
+            ext *= 10;
+            if (newValue > max) {
                 break;
             }
-            if (str.length() > 1 && str[0] == '0') {
-                continue;
+            if (add > 0 || ext == 10) {
+                a[i] += a[j - 1];
+                a[i] %= mod;
             }
-            a[i] += a[j - 1];
-            a[i] %= mod;
         }
     }
 
