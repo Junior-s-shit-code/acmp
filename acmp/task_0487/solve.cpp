7e69b75da7c8d91284666a10fedbe2759d29943b
Dirwul <pwgofriends@gmail.com>
2020-12-11 22:00:31 +0500

    task_487 accepted

---
+++ acmp/task_0487/solve.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+bool badMove(int n, int maxTake, int value) {
+    if (n <= maxTake && value < n) {
+        return true;
+    } else if (n > maxTake && n % (maxTake + 1) != 0 && (n - value) % (maxTake + 1) != 0) {
+        return true;
+    }
+    return false;
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int n, maxTake, nMoves;
+    scanf("%d %d %d", &n, &maxTake, &nMoves);
+    for (int i = 0; i < nMoves; i++) {
+        int value;
+        scanf("%d", &value);
+        if (badMove(n, maxTake, value)) {
+            printf("F\n");
+        } else {
+            printf("T\n");
+        }
+        n -= value;
+    }
+    return 0;
+}
