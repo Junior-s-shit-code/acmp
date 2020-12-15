09a85e6d905e7a7457049a78cc14ff69017ab165
Dirwul <pwgofriends@gmail.com>
2020-12-15 13:48:57 +0500

    remove useless headers and const

--- acmp/task_0798/solve.cpp
+++ acmp/task_0798/solve.cpp
@@ -1,11 +1,8 @@
 #include <stdio.h>
-#include <string>
-#include <vector>
 
 int main() {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
-    const int BLACK = 0;
     int sizeI, sizeJ, i, j, type;
     scanf("%d %d %d %d %d", &sizeI, &sizeJ, &i, &j, &type);
     if (((sizeI * sizeJ) & 1) == 1) {
