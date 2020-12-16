db78693cd9f69e726642f3a156ea8506b508b991
Dirwul <pwgofriends@gmail.com>
2020-12-16 22:12:05 +0500

    acmp+854 accepted

---
+++ acmp/task_0854/solve.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <string>
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int start, end;
+    char buf[1 + 20];
+    scanf("%d %d %s", &start, &end, &buf);
+    std::string type(buf);
+    if (type == "auto" ||
+        type == "freeze" && end < start ||
+        type == "heat" && start < end) {
+        printf("%d", end);
+    } else {
+        printf("%d", start);
+    }
+    return 0;
+}
