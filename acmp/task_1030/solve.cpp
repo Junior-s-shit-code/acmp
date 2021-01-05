7248a1cb7daacb6ad7c27867a31ae541e28e6683
Dirwul <pwgofriends@gmail.com>
2021-01-05 22:43:22 +0500

    fix (?) codestyle

--- acmp/task_1030/solve.cpp
+++ acmp/task_1030/solve.cpp
@@ -8,6 +8,6 @@ int main() {
     long long a, b, c, d;
     scanf("%lld %lld %lld %lld", &a, &b, &c, &d);
     long double sum = std::min(a, b) + std::min(c, d);
-    printf("%d", int(std::sqrt(sum)));
+    printf("%d", (int) std::sqrt(sum));
     return 0;
 }
