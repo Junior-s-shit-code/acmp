e423fe772c63bbea76241049208fb5dda2484f49
Dirwul <pwgofriends@gmail.com>
2021-01-08 05:15:50 +0500

    simpler solve

--- acmp/task_0487/solve.cpp
+++ acmp/task_0487/solve.cpp
@@ -1,24 +1,19 @@
 #include <stdio.h>
 
-bool badMove(int n, int maxTake, int value) {
-    return n <= maxTake && value < n ||
-        n % (maxTake + 1) != 0 && (n - value) % (maxTake + 1) != 0;
-}
-
 int main() {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
     int n, maxTake, nMoves;
     scanf("%d %d %d", &n, &maxTake, &nMoves);
     for (int i = 0; i < nMoves; i++) {
-        int value;
-        scanf("%d", &value);
-        if (badMove(n, maxTake, value)) {
+        int curMove;
+        scanf("%d", &curMove);
+        if (n % (maxTake + 1) != 0 && curMove != n % (maxTake + 1)) {
             printf("F\n");
         } else {
             printf("T\n");
         }
-        n -= value;
+        n -= curMove;
     }
     return 0;
 }
