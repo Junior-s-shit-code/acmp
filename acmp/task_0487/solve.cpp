f8fd7f3f82bfe7a6f307c9999ea1b4d61287aa8d
Dirwul <pwgofriends@gmail.com>
2021-01-08 05:12:08 +0500

    fix code-style

--- acmp/task_0487/solve.cpp
+++ acmp/task_0487/solve.cpp
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
 bool badMove(int n, int maxTake, int value) {
-    if (n <= maxTake && value < n ||
-        n % (maxTake + 1) != 0 && (n - value) % (maxTake + 1) != 0) {
-        return true;
-    } 
-    return false;
+    return n <= maxTake && value < n ||
+        n % (maxTake + 1) != 0 && (n - value) % (maxTake + 1) != 0;
 }
 
 int main() {
