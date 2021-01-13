64cbe64b67c80251a36901609f798dfa0648b459
Dirwul <pwgofriends@gmail.com>
2021-01-13 20:51:50 +0500

    acmp_1515 accepted

---
+++ acmp/task_1515/solve.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    long long n, k;
+    scanf("%lld %lld", &n, &k);
+    int ans = 0;
+    while (n >= k) {
+        ans++;
+        if (n % k == 0) {
+            printf("%d", ans);
+            return 0;
+        }
+        n = n - n / k;
+    }
+    printf("0");
+    return 0;
+}
