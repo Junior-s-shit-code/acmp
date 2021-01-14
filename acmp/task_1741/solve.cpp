8ee5de0fd0c7feeacb5e0a411faa28b2588a332c
Dirwul <pwgofriends@gmail.com>
2021-01-14 22:38:23 +0500

    acmp_1741 accepted

---
+++ acmp/task_1741/solve.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <vector>
+
+bool canDrive(
+    double time,
+    const int &fineId,
+    const int &nSegments,
+    const std::vector<int> &maxSegmentSpeed,
+    const std::vector<int> &speedLimits,
+    const std::vector<int> &segmentLen
+) {
+    for (int i = 0; i < nSegments; i++) {
+        int curSpeed = maxSegmentSpeed[i] + speedLimits[fineId];
+        time -= 1.0 * segmentLen[i] / curSpeed;
+    }
+    return time >= 0;
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int nSegments;
+    scanf("%d", &nSegments);
+    std::vector<int> maxSegmentSpeed(nSegments);
+    for (int i = 0; i < nSegments; i++) {
+        scanf("%d", &maxSegmentSpeed[i]);
+    }
+    std::vector<int> segmentLen(nSegments);
+    for (int i = 0; i < nSegments; i++) {
+        scanf("%d", &segmentLen[i]);
+    }
+    int nSpeedLimits;
+    scanf("%d", &nSpeedLimits);
+    std::vector<int> speedLimits(nSpeedLimits, 0);
+    for (int i = 1; i < nSpeedLimits; i++) {
+        scanf("%d", &speedLimits[i]);
+    }
+    std::vector<int> violationCost(nSpeedLimits + 1, 0);
+    for (int i = 1; i <= nSpeedLimits; i++) {
+        scanf("%d", &violationCost[i]);
+    }
+    int nQ;
+    scanf("%d", &nQ);
+    for (int q = 0; q < nQ; q++) {
+        int start, end;
+        scanf("%d %d", &start, &end);
+        int time = end - start;
+        int left = 0;
+        int right = nSpeedLimits;
+        while (left < right) {
+            int mid = (left + right) >> 1;
+            if (canDrive(time, mid, nSegments, maxSegmentSpeed, speedLimits, segmentLen)) { 
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        int maxCost = violationCost[left];
+        printf("%d\n", maxCost);
+    }
+    return 0;
+}
