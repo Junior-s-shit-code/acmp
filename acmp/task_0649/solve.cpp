de1917b53f09feb95e7c3e348d92b60ccf237b0f
Dirwul <pwgofriends@gmail.com>
2021-02-11 03:38:47 +0500

    remove long long cast and 'str' renamed 's'

--- acmp/task_0649/solve.cpp
+++ acmp/task_0649/solve.cpp
@@ -9,17 +9,17 @@ int main() {
     scanf("%d %d", &n, &maxEqual);
     char buf[1 + (int)1e6];
     scanf("%s", &buf);
-    std::string str(buf);
+    std::string s(buf);
     std::unordered_map<char, int> equal;
     long long ans = 0;
     int right = -1;
     for (int left = 0; left < n; left++) {
-        while (right + 1 < n && equal[str[right + 1]] < maxEqual) {
+        while (right + 1 < n && equal[s[right + 1]] < maxEqual) {
             right++;
-            equal[str[right]]++;
+            equal[s[right]]++;
         }
-        ans += (long long) right - left + 1;
-        equal[str[left]]--;
+        ans += right - left + 1;
+        equal[s[left]]--;
     }
     printf("%lld", ans);
     return 0;
