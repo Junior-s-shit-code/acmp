0b76f6d0a6e822a839fe3503d02da8a14990df47
Dirwul <pwgofriends@gmail.com>
2021-02-10 20:11:18 +0500

    acmp_76 accepted

---
+++ acmp/task_0076/solve.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    const long long MAX_TIME = 24 * 60;
+    int n;
+    scanf("%d", &n);
+    std::vector<int> in(MAX_TIME, 0);
+    std::vector<int> out(MAX_TIME, 0);
+    for (int i = 0; i < n; i++) {
+        int h, m;
+        scanf("%d:%d", &h, &m);
+        int curTime = h * 60 + m;
+        in[curTime]++;
+        scanf("%d:%d", &h, &m);
+        curTime = h * 60 + m;
+        out[curTime]++;
+    }
+    int maxVisitors = 0;
+    int curVisitors = 0;
+    for (int i = 0; i < MAX_TIME; i++) {
+        curVisitors += in[i];
+        maxVisitors = std::max(maxVisitors, curVisitors);
+        curVisitors -= out[i];
+    }
+    printf("%d", maxVisitors);
+    return 0;
+} 
