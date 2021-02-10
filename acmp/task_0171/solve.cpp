c75c4da9eb2975fd3a2774ac12ca59839564e195
Dirwul <pwgofriends@gmail.com>
2021-02-11 03:22:34 +0500

    fix style

--- acmp/task_0171/solve.cpp
+++ acmp/task_0171/solve.cpp
@@ -4,17 +4,17 @@
 int main() {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
-    const int maxDivisors = 1000;
+    const int MAX_DIVISORS = 1000;
     long long num;
     scanf("%lld", &num);
     long long ans = 1;
-    for (int d = 2; d < maxDivisors; d++) {
-        int count = 1;
+    for (int d = 2; d < MAX_DIVISORS; d++) {
+        int count = 0;
         while (num % d == 0) {
             num /= d;
             count++;
         }
-        ans *= count;
+        ans *= count + 1;
     }
     printf("%lld", ans);
     return 0;
