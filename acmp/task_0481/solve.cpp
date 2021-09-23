38f91bea4430dc01edf97ee7aff546318737293e
Dirwul <sottotcgr@gmail.com>
2021-09-23 19:05:43 +0300

    fix code-style

--- acmp/task_0481/solve.cpp
+++ acmp/task_0481/solve.cpp
@@ -6,25 +6,22 @@
 int main() {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
-    char buf[1+30];
+    char buf[1 + 30];
     scanf("%s", &buf);
     std::string s(buf);
     int n = (int)s.length();
-    std::vector<std::vector<int>> a(n, std::vector<int> (n, 0));
+    std::vector<std::vector<int>> a(n, std::vector<int>(n, 0));
     for (int len = 1; len <= n; len++) { 
         for (int startPos = 0; startPos + len <= n; startPos++) {
             int endPos = startPos + len - 1;
             if (len == 1) {
                 a[startPos][endPos] = 1;
-                continue;
-            }
-            int left = std::min(startPos + 1, endPos);
-            int right = std::max(startPos, endPos - 1);
-            
-            if (s[startPos] != s[endPos]) {
-                a[startPos][endPos] = a[startPos][right] + a[left][endPos] - a[left][right];
-            } else if (s[startPos] == s[endPos]) {
-                a[startPos][endPos] = a[startPos][right] + a[left][endPos] + 1;
+            } else if (len == 2) {
+                a[startPos][endPos] = (s[startPos] == s[endPos] ? 3 : 2);
+            } else if (s[startPos] != s[endPos]) {
+                a[startPos][endPos] = a[startPos][endPos - 1] + a[startPos + 1][endPos] - a[startPos + 1][endPos - 1];
+            } else {
+                a[startPos][endPos] = a[startPos][endPos - 1] + a[startPos + 1][endPos] + 1;
             }
         }
     }
