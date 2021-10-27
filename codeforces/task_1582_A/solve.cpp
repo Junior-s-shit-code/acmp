4a6d3328efe7dbb2dc684f8cccc85a68dd1319fe
Dirwul <sottotcgr@gmail.com>
2021-10-27 16:34:50 +0300

    codeforces_1582_A accepted

---
+++ codeforces/task_1582_A/solve.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        int a, b, c;
+        scanf("%d %d %d", &a, &b, &c);
+        long long t = a + 2 * b + 3 * c;
+        printf("%d\n", t & 1 > 0 ? 1 : 0);
+    }
+    return 0;
+}
