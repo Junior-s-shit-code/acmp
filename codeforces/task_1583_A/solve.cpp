6e8a03c6ebac94a51981951f273384e114455211
Dirwul <sottotcgr@gmail.com>
2021-11-08 09:28:00 +0300

    fix code-style

--- codeforces/task_1583_A/solve.cpp
+++ codeforces/task_1583_A/solve.cpp
@@ -7,43 +7,48 @@ bool isPrime(int num) {
             return false;
         }
     }
-    return true;
+    return num > 1;
+}
+
+void solveTest() {
+    int sum = 0;
+    int minOdd = (int)1e9;
+    int n;
+    int iNum = 0;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        int value;
+        scanf("%d", &value);
+        sum += value;
+        if ((value & 1) > 0) {
+            minOdd = value;
+            iNum = i + 1;
+        }
+    }
+    if (!isPrime(sum)) {
+        printf("%d\n", n);
+        for (int i = 1; i <= n; i++) {
+            printf("%d ", i);
+        }
+        printf("\n");
+    } else {
+        printf("%d\n", n - 1);
+        for (int i = 1; i <= n; i++) {
+            if (i != iNum) {
+                printf("%d ", i);
+            }
+        }
+        printf("\n");
+    }
 }
 
 int main() {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
-    const int INF = (int)1e9;
     int nT;
     scanf("%d", &nT);
     for (int t = 0; t < nT; t++) {
-        int sum = 0;
-        int minOdd = INF;
-        int n, value, index = 0;
-        scanf("%d", &n);
-        for (int i = 0; i < n; i++) {
-            scanf("%d", &value);
-            sum += value;
-            if ((value & 1) > 0) {
-                minOdd = value;
-                index = i + 1;
-            }
-        }
-        if (!isPrime(sum)) {
-            printf("%d\n", n);
-            for (int i = 1; i <= n; i++) {
-                printf("%d ", i);
-            }
-            printf("\n");
-        } else {
-            printf("%d\n", n - 1);
-            for (int i = 1; i <= n; i++) {
-                if (i != index) {
-                    printf("%d ", i);
-                }
-            }
-            printf("\n");
-        }
+        solveTest();
     }
     return 0;
 }
