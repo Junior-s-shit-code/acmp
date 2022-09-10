42f98fe1e3a59f2a89a85833031455dbbe364642
Sorqen <SottoTCGR@gmail.com>
2022-09-10 13:27:18 +0300

    accepted

---
+++ acmp/task_1231/Main.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    Scanner in;
+
+    PrintWriter out;
+
+    private void solve() {
+        int n = in.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = in.nextInt();
+        }
+        int count = 0;
+        for (int curN = n; curN > 1; curN--) {
+            for (int i = 0; i + 1 < curN; i++) {
+                if (a[i] > a[i + 1]) {
+                    int tmp = a[i];
+                    a[i] = a[i + 1];
+                    a[i + 1] = tmp;
+                    count++;
+                }
+            }
+        }
+        out.println(count);
+    }
+
+    public void run() {
+        try {
+            Locale.setDefault(Locale.US);
+            in = new Scanner(new File("input.txt"));
+            out = new PrintWriter("output.txt");
+
+            solve();
+
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Main().run();
+    }
+}
